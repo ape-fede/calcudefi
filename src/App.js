6534f51a8f8611472bf86d87be8bdc867f2ade9a
fedestraus <fede_straus@hotmail.com>
2021-09-18 12:34:39 -0300

    bugfix ui

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
 	})
 	return (
 		<ThemeProvider theme={darkTheme}>
-			<Paper style={{height: '100vh'}}>
+			<Paper style={{minHeight: '100vh'}}>
 				<Header theme={theme} setTheme={setTheme} />
 				<Grid
 					container
